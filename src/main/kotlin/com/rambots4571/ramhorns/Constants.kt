28042541de3d447e3f4527efb7b26a36aa28dcb7
Mahim <mahim216@gmail.com>
2019-01-28 12:16:40 -0500

    Update Constants.kt

--- src/main/kotlin/com/rambots4571/ramhorns/Constants.kt
+++ src/main/kotlin/com/rambots4571/ramhorns/Constants.kt
@@ -3,12 +3,11 @@ package com.rambots4571.ramhorns
 object Constants {
     object Talon {
         const val timeoutMs = 100
-        const val timeout = 0.1
+        const val trajectoryPointPeriod = 100
+        const val longTimeoutMs = 1000
         const val MIN_POINTS_IN_TALON = 5
         const val MAX_BTM_BUFFER_COUNT = 128
         const val MAX_TOP_BUFFER_COUNT = 512
-        const val MOTION_CONTROL_FRAME_PERIOD = timeoutMs / 2
-        const val PIDSlotIdx = 0
     }
 
     object Paths {
