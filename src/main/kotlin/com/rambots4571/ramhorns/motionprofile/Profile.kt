e5fd18c59b9cbf217919023257edb8353e5e28c6
Mahim <mahim216@gmail.com>
2019-01-28 12:17:48 -0500

    adding profile and handler

---
+++ src/main/kotlin/com/rambots4571/ramhorns/motionprofile/Profile.kt
@@ -0,0 +1,39 @@
+package com.rambots4571.ramhorns.motionprofile
+
+import com.ctre.phoenix.motion.TrajectoryPoint
+import com.rambots4571.ramhorns.Constants
+import com.rambots4571.ramhorns.hardware.LazyTalonSRX
+import edu.wpi.first.wpilibj.DriverStation
+
+class Profile(
+    pathName: String, leftTalon: LazyTalonSRX, rightTalon: LazyTalonSRX, leftPIDSlot: Int = 0, rightPIDSlot: Int = 0) {
+    val length: Int
+    val handler: Handler
+    val leftProfile: ArrayList<TrajectoryPoint>
+    val rightProfile: ArrayList<TrajectoryPoint>
+    var timoutMs = Constants.Talon.timeoutMs
+    var trajectoryPointPeriod = Constants.Talon.trajectoryPointPeriod
+    var motionControlFramePeriod = timoutMs / 2
+
+    init {
+        leftProfile = Parser.getPoints(pathName + Constants.Paths.leftSuffix)
+        rightProfile = Parser.getPoints(pathName + Constants.Paths.rightSuffix)
+        length = leftProfile.size
+        handler = Handler(this, leftTalon, rightTalon)
+    }
+
+    val isFinished = handler.isFinished
+
+    fun execute() {
+        if (leftProfile.size != 0 && rightProfile.size != 0) {
+            handler.execute()
+        } else {
+            DriverStation.getInstance()
+            DriverStation.reportError("Tried to run empty profile!", false)
+        }
+    }
+
+    fun onInterrupt() {
+        handler.onInterrupt()
+    }
+}
