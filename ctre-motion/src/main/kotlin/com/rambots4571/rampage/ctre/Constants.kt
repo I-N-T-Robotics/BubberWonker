0212ad5cd3862b47578c36cd229a9194afbc9d74
Mahim <mahim216@gmail.com>
2019-02-09 12:11:39 -0500

    Update Constants.kt

--- ctre-motion/src/main/kotlin/com/rambots4571/rampage/ctre/Constants.kt
+++ ctre-motion/src/main/kotlin/com/rambots4571/rampage/ctre/Constants.kt
@@ -2,9 +2,9 @@ package com.rambots4571.rampage.ctre
 
 object Constants {
     object Talon {
-        const val timeoutMs = 100
-        const val trajectoryPointPeriod = 100
-        const val longTimeoutMs = 1000
+        const val timeoutMs = 10
+        const val trajectoryPointPeriod = 10
+        const val longTimeoutMs = 10
         const val MIN_POINTS_IN_TALON = 5
         const val MAX_BTM_BUFFER_COUNT = 128
         const val MAX_TOP_BUFFER_COUNT = 512
