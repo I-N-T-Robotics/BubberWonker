8361e0a33bdda9952e19428a153db09ee351d2bd
Mahim <mahim216@gmail.com>
2020-02-09 17:21:54 -0500

    fixed toggle command?

--- src/main/kotlin/com/rambots4571/rampage/joystick/Button.kt
+++ src/main/kotlin/com/rambots4571/rampage/joystick/Button.kt
@@ -1,20 +1,36 @@
 package com.rambots4571.rampage.joystick
 
 import edu.wpi.first.wpilibj.GenericHID
+import edu.wpi.first.wpilibj2.command.InstantCommand
+import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
 
 class Button(joystick: GenericHID, buttonNumber: Int) :
         JoystickButton(joystick, buttonNumber) {
-    private var toggle = false
-
-    fun toggle(firstAction: Runnable, secondAction: Runnable) : Button {
-        whenPressed(Runnable {
-            if (toggle) {
-                secondAction.run()
-            } else {
-                firstAction.run()
+    fun toggle(
+        action: Runnable, defaultAction: Runnable,
+        vararg subsystem: SubsystemBase): Button {
+        var toggle = false
+        whenPressed(object : InstantCommand() {
+            init {
+                requirements.addAll(subsystem)
             }
-            toggle = !toggle
+
+            override fun initialize() {
+                if (toggle) {
+                    defaultAction.run()
+                } else {
+                    action.run()
+                }
+                toggle = !toggle
+            }
+
+            override fun end(interrupted: Boolean) {
+                if (interrupted) {
+                    toggle = false
+                    cancel()
+                }
+            }
         })
         return this
     }
