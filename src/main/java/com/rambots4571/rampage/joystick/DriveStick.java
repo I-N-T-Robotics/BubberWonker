3b46fc1859331a20cf9e2a1e2c9444141a1f8140
Mahim <mahim216@gmail.com>
2022-02-21 02:54:16 -0500

    changing getAxis() -> getAxisValue()

--- src/main/java/com/rambots4571/rampage/joystick/DriveStick.java
+++ src/main/java/com/rambots4571/rampage/joystick/DriveStick.java
@@ -4,71 +4,74 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class DriveStick extends Joystick {
-    private final Buttons<DriveStick.ButtonType> buttons;
+	private final Buttons<DriveStick.ButtonType> buttons;
 
-    public enum ButtonType implements Mappable {
-        button1(1),
-        button2(2),
-        button3(3),
-        button4(4),
-        button5(5),
-        button6(6),
-        button7(7),
-        button8(8),
-        button9(9),
-        button10(10),
-        button11(11),
-        button12(12);
+	public enum ButtonType implements Mappable {
+		button1(1),
+		button2(2),
+		button3(3),
+		button4(4),
+		button5(5),
+		button6(6),
+		button7(7),
+		button8(8),
+		button9(9),
+		button10(10),
+		button11(11),
+		button12(12);
 
-        private final int id;
+		private final int id;
 
-        ButtonType(int id) {
-            this.id = id;
-        }
+		ButtonType(int id) {
+			this.id = id;
+		}
 
-        @Override
-        public int getID() {
-            return id;
-        }
-    }
+		@Override
+		public int getID() {
+			return id;
+		}
+	}
 
-    public enum Axis implements Mappable, Invertible {
-        xAxis(0), yAxis(1, true), zAxis(2), Slider(3);
+	public enum Axis implements Mappable, Invertible {
+		xAxis(0),
+		yAxis(1, true),
+		zAxis(2),
+		Slider(3);
 
-        private final int id;
-        private final boolean inverted;
+		private final int id;
+		private final boolean inverted;
 
-        Axis(int id, boolean inverted) {
-            this.id = id;
-            this.inverted = inverted;
-        }
+		Axis(int id, boolean inverted) {
+			this.id = id;
+			this.inverted = inverted;
+		}
 
-        Axis(int id) {
-            this.id = id;
-            this.inverted = false;
-        }
+		Axis(int id) {
+			this.id = id;
+			this.inverted = false;
+		}
 
-        @Override
-        public boolean isInverted() {
-            return false;
-        }
+		@Override
+		public boolean isInverted() {
+			return false;
+		}
 
-        @Override
-        public int getID() {
-            return 0;
-        }
-    }
+		@Override
+		public int getID() {
+			return 0;
+		}
+	}
 
-    public DriveStick(int port) {
-        super(port);
-        buttons = new Buttons<>(this);
-    }
+	public DriveStick(int port) {
+		super(port);
+		buttons = new Buttons<>(this);
+	}
 
-    public JoystickButton getButton(DriveStick.ButtonType button) {
-        return buttons.get(button);
-    }
+	public JoystickButton getButton(DriveStick.ButtonType button) {
+		return buttons.get(button);
+	}
 
-    public double getAxis(DriveStick.Axis axis) {
-        return axis.inverted ? -getRawAxis(axis.id) : getRawAxis(axis.id);
-    }
+	public double getAxisValue(DriveStick.Axis axis) {
+		return axis.inverted ? -getRawAxis(axis.id) : getRawAxis(axis.id);
+	}
 }
--- src/main/java/com/rambots4571/rampage/joystick/Gamepad.java
+++ src/main/java/com/rambots4571/rampage/joystick/Gamepad.java
@@ -4,82 +4,83 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Gamepad extends Joystick {
-    private final Buttons<Gamepad.ButtonType> buttons;
+	private final Buttons<Gamepad.ButtonType> buttons;
 
-    public enum ButtonType implements Mappable {
-        A(1),
-        B(2),
-        X(3),
-        Y(4),
-        LeftBumper(5),
-        RightBumper(6),
-        BackButton(7),
-        StartButton(8),
-        LeftStick(9),
-        RightStick(10);
+	public enum ButtonType implements Mappable {
+		A(1),
+		B(2),
+		X(3),
+		Y(4),
+		LeftBumper(5),
+		RightBumper(6),
+		BackButton(7),
+		StartButton(8),
+		LeftStick(9),
+		RightStick(10);
 
-        private final int id;
+		private final int id;
 
-        ButtonType(int id) {
-            this.id = id;
-        }
+		ButtonType(int id) {
+			this.id = id;
+		}
 
-        @Override
-        public int getID() {
-            return id;
-        }
+		@Override
+		public int getID() {
+			return id;
+		}
 
-    }
+	}
 
-    public enum Axis implements Mappable, Invertible {
-        LeftXAxis(0),
-        LeftYAxis(1, true),
-        LeftTrigger(2),
-        RightTrigger(3),
-        RightXAxis(4),
-        RightYAxis(5, true);
+	public enum Axis implements Mappable, Invertible {
+		LeftXAxis(0),
+		LeftYAxis(1, true),
+		LeftTrigger(2),
+		RightTrigger(3),
+		RightXAxis(4),
+		RightYAxis(5, true);
 
-        private final int id;
-        private final boolean inverted;
+		private final int id;
+		private final boolean inverted;
 
-        Axis(int id, boolean inverted) {
-            this.id = id;
-            this.inverted = inverted;
-        }
+		Axis(int id, boolean inverted) {
+			this.id = id;
+			this.inverted = inverted;
+		}
 
-        Axis(int id) {
-            this.id = id;
-            this.inverted = false;
-        }
+		Axis(int id) {
+			this.id = id;
+			this.inverted = false;
+		}
 
-        @Override
-        public int getID() {
-            return id;
-        }
+		@Override
+		public int getID() {
+			return id;
+		}
 
-        @Override
-        public boolean isInverted() {
-            return inverted;
-        }
-    }
+		@Override
+		public boolean isInverted() {
+			return inverted;
+		}
+	}
 
-    /**
-     * Construct an instance of a joystick. The joystick index is the USB
-     * port on the drivers
-     * station.
-     *
-     * @param port The port on the Driver Station that the joystick is plugged into.
-     */
-    public Gamepad(int port) {
-        super(port);
-        buttons = new Buttons<>(this);
-    }
+	/**
+	 * Construct an instance of a joystick. The joystick index is the USB
+	 * port on the drivers
+	 * station.
+	 *
+	 * @param port The port on the Driver Station that the joystick is plugged
+	 *             into.
+	 */
+	public Gamepad(int port) {
+		super(port);
+		buttons = new Buttons<>(this);
+	}
 
-    public JoystickButton getButton(Gamepad.ButtonType button) {
-        return buttons.get(button);
-    }
+	public JoystickButton getButton(Gamepad.ButtonType button) {
+		return buttons.get(button);
+	}
 
-    public double getAxis(Gamepad.Axis axis) {
-        return axis.inverted ? -getRawAxis(axis.id) : getRawAxis(axis.id);
-    }
+	public double getAxisValue(Gamepad.Axis axis) {
+		return axis.inverted ? -getRawAxis(axis.id) : getRawAxis(axis.id);
+	}
 }
