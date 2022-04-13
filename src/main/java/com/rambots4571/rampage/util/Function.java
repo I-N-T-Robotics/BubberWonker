70976cf3ec92369dd65f35857ba3975fd82cc9eb
Mahim <mahim216@gmail.com>
2022-04-12 21:08:35 -0400

    adding helper methods

---
+++ src/main/java/com/rambots4571/rampage/util/Function.java
@@ -0,0 +1,22 @@
+package com.rambots4571.rampage.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Function {
+	@SafeVarargs
+	public static <T> List<T> applyAll(Consumer<T> apply, T... t) {
+		List<T> list = Arrays.asList(t);
+		list.forEach(apply);
+		return list;
+	}
+
+	@SafeVarargs
+	public static <T> List<T> combine(List<T>... lists) {
+		List<T> list = new ArrayList<>();
+		Arrays.asList(lists).forEach(list::addAll);
+		return list;
+	}
+}
